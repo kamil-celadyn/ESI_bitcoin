bd1f45bc9dfb15137b2c6200c72fd5a180357c7c
JKG721 <kubagorczyca100@gmail.com>
2026-04-15 23:12:17 +0200

    Delete DATA_CLEAN.py
    usunięcie data_clean żeby podmienić na nowszą wersje

--- DATA_CLEAN.py
+++
@@ -1,6 +0,0 @@
-import pandas as pd
-df = pd.read_csv('dane_regresja.csv')
-new_df = df.dropna()
-nowa_kolejnosc = ['Open', 'High', 'Low', 'Volume', 'Close']
-df = df[nowa_kolejnosc]
-df.to_csv('dane_regresja.csv', index=False)
